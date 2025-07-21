fee325785cfef8e90018c2786b4e88cd8bb31046
Marinegrn <marinegarein@outlook.fr>
2025-07-21 15:47:22 +0200

    update : upload Frontend CV

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
             const link = document.createElement('a');
             
             // Fichier dans le dossier assets
-            link.href = '/assets/CV_MarineGAREIN_finBootcamp.pdf';
+            link.href = '/assets/MarineGAREIN_FrontEnd_CV.pdf';
             link.download = 'Marine_Garein_CV.pdf';
             link.style.display = 'none';
 
